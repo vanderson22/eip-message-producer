23df9011b768bdd3586f3fa9a1ef896c53785ba5
Vanderson Nogueira <vanderson.santos@prill.com.br>
2025-04-14 11:25:55 -0300

    feat: inclusao producer

--- src/producer/producer.service.ts
+++ src/producer/producer.service.ts
@@ -8,19 +8,24 @@ import { throwError } from 'rxjs';
 export class ProducerService {
   constructor(@Inject('RABBITMQ_SERVICE') private readonly client: ClientProxy) {}
 
-  async sendMessage(queue: string, payload: SendMessageDto , retryParam: number = 3, timeoutParam: number = 5000): Promise<void> {
-   
-    console.log('Enviando mensagem : ', JSON.stringify(payload));
+  async sendMessage(
+    queue: string,
+    payload: SendMessageDto,
+    retryParam = 3,
+    timeoutParam = 5000,
+  ): Promise<void> {
+    console.log('Enviando mensagem para a fila:', queue);
+  
     return this.client
-      .emit(queue, payload)
+      .emit(queue, { data: payload.message }) // `queue` e `pattern` precisam ser iguais
       .pipe(
-        timeout(timeoutParam),        
-        retry(retryParam),            
+        timeout(timeoutParam),
+        retry(retryParam),
         catchError((err) => {
           console.error('Erro ao enviar mensagem:', err.message);
           return throwError(() => err);
         }),
       )
-       .toPromise();  
+      .toPromise();
   }
 }
